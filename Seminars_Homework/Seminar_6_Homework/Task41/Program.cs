678b33a9b391e7c87cdae437425ac2429ef872d1
Denis <Den00736@mail.ru>
2023-02-13 01:32:40 +0400

    начало домашнего задания

---
+++ Seminars_Homework/Seminar_6_Homework/Task41/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+int PlusNum(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+
+// Console.Write("Введите числа: ");
+int[] array = GetArray(10, -10, 10);
+Console.WriteLine(string.Join(", ", array));
+int result = PlusNum(array);
+Console.WriteLine($"количество положительных чисел в введенном массиве равно : {result}");
