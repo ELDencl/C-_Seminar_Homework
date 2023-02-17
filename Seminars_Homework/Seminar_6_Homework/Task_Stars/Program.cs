0e844c85efcabce454e5d8d29a2c2157816e74d5
Denis_Kormishkov <den00736@mail.ru>
2023-02-17 04:24:56 +0400

    Домашка 6 Семинара

--- Seminars_Homework/Seminar_6_Homework/Task_Stars/Program.cs
+++ Seminars_Homework/Seminar_6_Homework/Task_Stars/Program.cs
@@ -6,3 +6,27 @@
 // 4 5 6 -> 7 8 9 6 3 2 1 4 5
 // 7 8 9
 
+
+int[,] array = new int[,]
+{
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9},
+};
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }   
+    
+}
+
+
+
+PrintArray(array);
