a5cf5283eb5a85aa5ca72110346a128e3b4c9a63
Denis_Kormishkov <den00736@mail.ru>
2023-02-17 04:04:28 +0400

    Лекция №4

---
+++ Lection4/Task1/Program.cs
@@ -0,0 +1,66 @@
+﻿
+
+
+string[,] table = new string[2, 5];  // при индексах [2,5] у нас будут строки (от 0 до 1)(2 штуки)  и столбцы(от 0 до 4)(5 штук)
+// String.Empty инициализация строки 
+// table[0,0]  table[0,1]  table[0,2]  table[0,3]  table[0,4]  
+// table[1,0]  table[1,1]  table[1,2]  table[1,3]  table[1,4]
+
+// table[1,2]="слово";
+// // пишем цикл в цикле для распечатывания массива
+// for (int rows = 0;  rows < 2; rows++) // строки
+// {
+//     for (int columns = 0; columns < 5; columns++) // столбцы
+//     {
+//         Console.WriteLine($"-{table[rows,columns]}-");
+//     }
+// }
+
+
+// int[,] matrix = new int[3, 4];
+// // пишем цикл в цикле для распечатывания массива
+// for (int rows = 0; rows < matrix.GetLength(0); rows++)
+// {
+//     for (int columns = 0; columns < matrix.GetLength(1); columns++) // столбцы
+//     {
+//         Console.Write($"{matrix[rows, columns]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+
+
+
+void PrintArray(int[,] matr)
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++) // столбцы
+        {
+            Console.Write($"{matr[rows, columns]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);  // [1,10)
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+
+PrintArray(matrix);
+Console.WriteLine();
+
+FillArray(matrix);
+
+PrintArray(matrix);
---
+++ Lection4/TaskFactorial/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+
+
+
+double Factorial(int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+
+
+// for (int i = 1; i < 40; i++)
+// {
+//     Console.WriteLine(Factorial(i));
+// }
+
+// Console.WriteLine(Factorial(5)); // 1*2*3 = 6
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
+
+
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) +f(n-2)
+
+
+
+double Fibonachy(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonachy(n - 1) + Fibonachy(n - 2);
+}
+
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i} = {Fibonachy(i)}");
+}
