e1b53f275c761771b5e3a66c229a7ecfe6ec4f01
Denis_Kormishkov <den00736@mail.ru>
2023-02-21 02:07:19 +0400

    Seminar 7 Homework

---
+++ Seminars_Homework/Seminar_7_Homework/Task50/Program.cs
@@ -0,0 +1,78 @@
+﻿// Задача 50: Напишите программу, которая на вход 
+// принимает позиции элемента в двумерном массиве, и 
+// возвращает значение этого элемента или же указание, 
+// что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+Console.Clear();
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+
+
+
+
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t "); // \t табуляция (escape последовательсность) выравнивает столбцы 
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+void IndexElem(int[,] array, int i, int j)
+{
+    int num = 0;
+    if (i <= array.GetLength(0) && j <= array.GetLength(1))
+    {
+        num = array[i, j];
+        Console.WriteLine(num);
+    }
+    else
+    {
+        Console.WriteLine("такого элемента нет в массиве");
+    }
+}
+
+
+
+Console.Write("Введите количество строк: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов: ");
+int col = int.Parse(Console.ReadLine()!);
+
+int[,] array2D = GetArray(row, col, -10, 10);
+PrintArray(array2D);
+
+Console.Write("Введите строку искомого числа: ");
+int rowNum = int.Parse(Console.ReadLine()!);
+Console.Write("Введите столбец искомого числа: ");
+int colNum = int.Parse(Console.ReadLine()!);
+
+IndexElem(array2D, rowNum - 1, colNum - 1);
+
+
+
---
+++ Seminars_Homework/Seminar_7_Homework/Task52/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задача 52: Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом 
+// столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого 
+// столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] result = new double[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t "); // \t табуляция (escape последовательсность) выравнивает столбцы 
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+double[] AverageValueColumn(double[,] array)
+{
+    double[] sum = new double[array.GetLength(0)];
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum[j] += array[i,j];
+        }
+        
+        
+
+    }
+    for (int g = 0; g < array.GetLength(0); g++)
+    {
+        sum[g] /= array.GetLength(1);
+    }
+    return sum;
+}
+
+
+
+
+
+
+
+Console.Write("Введите количество строк: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов: ");
+int col = int.Parse(Console.ReadLine()!);
+
+double[,] array2D = GetArray(row, col, -10, 10);
+PrintArray(array2D);
+
+double[] sum =AverageValueColumn(array2D);
+Console.WriteLine(String.Join("; ", sum));
